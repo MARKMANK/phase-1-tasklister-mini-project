9a60a1df279eade385a93f2c4315fe67c21e12ea
kevinmarkman <kevin.w.markman@gmail.com>
2023-03-08 21:45:37 -0600

    Fixes the form code and adds delete alert

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector('form')
     form.addEventListener('submit',(event) => {
     event.preventDefault();
-    buildToDo(event.target.new_todo.value)
+    buildToDo(event.target.newtaskdescription.value)
     form.reset()
   })
 });
@@ -10,13 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
 function buildToDo(todo){
   let p = document.createElement('p')
   let btn = document.createElement('button')
-  btn.addEventListener('click'.handleDelete)
-  btn.textContent = 'x'
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = 'X'
   p.textContent = `${todo}  `
   p.appendChild(btn)
-  document.querySelector('#todo_container').appendChild(p)
+  document.querySelector('#list').appendChild(p)
 }
 
 function handleDelete(event){
   event.target.parentNode.remove()
+  alert("You did a thing!")
 }
