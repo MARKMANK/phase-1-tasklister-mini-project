0c8452c38996b7b8943075fa3ff1cd78559b2776
kevinmarkman <kevin.w.markman@gmail.com>
2023-03-08 21:15:33 -0600

    Adds fuction to add and delete items from the Todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+    form.addEventListener('submit',(event) => {
+    event.preventDefault();
+    buildToDo(event.target.new_todo.value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click'.handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo}  `
+  p.appendChild(btn)
+  document.querySelector('#todo_container').appendChild(p)
+}
+
+function handleDelete(event){
+  event.target.parentNode.remove()
+}
